5b9bbdb501af0da1cc28ddd44d1bc4d399ae23b2
michal-siniarski <siniarskim@gmail.com>
2025-12-03 12:54:24 +0100

    modify processing pipeline

--- src/main.py
+++ src/main.py
@@ -1,4 +1,69 @@
-################################################################################################################################
-#### main processing pipeline. When model is trained, run main.py with proper flags to process .LAZ files in specified directory.
-#### Pass main.py --help to get more info about flags.
-###############################################################################################################################
+from typing import Optional, Union
+import pathlib as pth
+import argparse
+import logging
+import tqdm
+
+import laspy
+import numpy as np
+
+from array_processing import SegmentClass
+
+
+def argparser():
+
+
+    parser = argparse.ArgumentParser(
+        description="Script for semantic segmentation of point clouds.\n"
+        "Supports .LAZ files (default). ",
+        formatter_class=argparse.RawTextHelpFormatter
+    )
+
+    parser.add_argument(
+        '--model_name',
+        type=str,
+        help=(
+            "Base of the model's name.\n"
+            "Use full model name without extension suffix (.pt file expected)"
+        )
+    )
+
+    
+    # Flag definition
+    parser.add_argument(
+        '--device',
+        type=str,
+        default='cpu',
+        choices=['cpu', 'cuda', 'gpu'], # choice limit
+        help=(
+            "Device for tensor based computation.\n"
+            "Pick 'cpu' or 'cuda'/ 'gpu'.\n"
+        )
+    )
+
+    parser.add_argument(
+        '--input_path',
+        type=str,
+        help=(
+            "Path to the directory with raw input files.\n"
+            "Supports .LAZ files by default. "
+        )
+    )
+
+    parser.add_argument(
+        '--mode',
+        type=int,
+        default=0,
+        choices=[0, 1], # choice limit
+        help=(
+            "Device for tensor based computation.\n"
+            'Pick:\n'
+            '0: test\n'
+            '1: process_files'
+        )
+    )
+
+    return parser.parse_args()
+
+if __name__ == "__main__":
+    main()
