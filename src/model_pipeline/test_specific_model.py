e314a2184156265748e7f42904f068ad9bbdc6c8
kalmary <58998639+kalmary@users.noreply.github.com>
2026-04-24 08:15:05 +0200

    testing specific model added

---
+++ src/model_pipeline/test_specific_model.py
@@ -0,0 +1,15 @@
+import json
+import torch
+from torchinfo import summary
+from model import RandLANet
+
+
+def model_info(config_path: str, n_classes: int, device: str = 'cpu'):
+    config = json.load(open(config_path))
+    model = RandLANet(config, n_classes=n_classes).to(device)
+    dummy = torch.zeros(6, 16384, config['d_in'], device=device)
+    summary(model, input_data=dummy, depth=3, col_names=["num_params", "trainable"])
+
+
+if __name__ == '__main__':
+    model_info('src/model_pipeline/model_configs/config_model_8.json', n_classes=10)
