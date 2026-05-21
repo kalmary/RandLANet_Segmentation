090139112bd5b096435ab5996b10a2d6a1805cb8
kalmary <siniarskim@gmail.com>
2026-05-21 15:30:56 +0200

    plotting update

--- src/utils/plot_laz.py
+++ src/utils/plot_laz.py
@@ -1,16 +1,49 @@
 from plot_cloud import plot_cloud
 import laspy
 import numpy as np
+import pathlib as pth
 
 
-def main():
-    file_path = "/mnt/SSD_EXT4_1TB/DATA/GRAJEWO_MINI_TEST/ITWL_Grajewo20_mini_mod.laz"
-    laz = laspy.read(file_path)
+def single_file():
+    laz = laspy.read("/mnt/SSD_EXT4_1TB/DATA/GRAJEWO_MINI_TEST/ITWL_Grajewo20_mini_mod.laz")
     points = np.vstack((laz.x, laz.y, laz.z)).T
-    intensity = laz.species
-    print(np.unique(intensity))
+    feature = laz.classification
+    
+    print("Plotting semantic classification")
+    plot_cloud(points, feature)
 
-    plot_cloud(points, intensity)
+    feature = laz.tree_ids
+    print("Plotting tree ids")
+    plot_cloud(points, feature)
+
+    feature = laz.species
+    print("Plotting species")
+    plot_cloud(points, feature)
+
+def mutliple_files():
+    
+    file_dir = pth.Path("/mnt/SSD_EXT4_1TB/DATA/GRAJEWO_MINI")
+
+    for file in file_dir.rglob("*.laz"):
+        if "_mod" not in file.stem:
+            continue
+            
+        laz = laspy.read(file)
+        points = np.vstack((laz.x, laz.y, laz.z)).T
+        feature = laz.classification
+        
+        print(f"Plotting {file.stem}, semantic classification")
+        plot_cloud(points, feature)
+
+
+        feature = laz.tree_ids
+        print(f"Plotting {file.stem}, tree ids")
+        plot_cloud(points, feature)
+
+        feature = laz.species
+        print(f"Plotting {file.stem}, species")
+        plot_cloud(points, feature)
+
 
 if __name__ == "__main__":
-    main()
+    single_file()
